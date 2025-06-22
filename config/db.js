c8736e88044778b6d778126af29671dcfbe14bae
Asish-Ray <aray19069@gmail.com>
2025-06-23 00:55:18 +0530

    Final version for Vercel deployment

--- config/db.js
+++ config/db.js
@@ -1,17 +1,16 @@
-import pg from 'pg';
+// config/db.js
+import pkg from 'pg';
 import dotenv from 'dotenv';
+
 dotenv.config();
-const { Pool } = pg;
-console.log("DB_USER:", process.env.DB_USER);
-console.log("DB_PASSWORD:", process.env.DB_PASSWORD);
-console.log("Connecting with DB password:", typeof process.env.DB_PASSWORD);
+
+const { Pool } = pkg;
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, // Required for Supabase
+  }
 });
 
 export default pool;
